1b8eb7d8f60371e6e9c11d40d4d6cb65b3b0f289
WANG WEI WEI <vivlanwong120729@gmail.com>
2017-12-03 20:17:38 +0800

    feat:update navbar menu padding

--- webapp/app/components/app.vue
+++ webapp/app/components/app.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <nav class="navbar navbar-expand navbar-dark bg-dark fixed-top">
             <a class="navbar-brand" href="#">Nicety</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                                   <span class="navbar-toggler-icon"></span>
