b03cf151ce866f51e837e0bcc8418bd4debd1d84
WANG WEI WEI <vivlanwong120729@gmail.com>
2017-12-06 18:45:05 +0800

    NIC-16 feat:add title card demo page

--- webapp/app/pages/Home.vue
+++ webapp/app/pages/Home.vue
@@ -5,15 +5,18 @@
         <div class="card-header">
           <div class="card-title">Special title treatment</div>
           <div class="card-control">
-            <button class="btn"><i class="ti-reload icon-lg"></i></button>
+            <button class="btn" data-toggle="collapse" data-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1"><i class="ti-angle-down icon-lg" ></i></button>
+            <button class="btn" ><i class="ti-reload icon-lg" ></i></button>
             <button class="btn"><i class="ti-close icon-lg"></i></button>
           </div>
         </div>
+          <div class="collapse multi-collapse" id="multiCollapseExample1">
         <div class="card-body">
           <i class="psi-align-right"></i>
           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
+          </div>
       </div>
     </div>
     <div class="col-lg-4">
@@ -40,6 +43,9 @@
   export default {
     components: {
       //   MainLayout
+    },
+     mounted: function () {//渲染完成
+     $('.collapse').collapse()
     }
   }
 </script>
