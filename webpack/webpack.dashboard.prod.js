771a15284e32cc93428dce67b6fd96003ad0b07a
WANG WEI WEI <vivlanwong120729@gmail.com>
2018-03-21 22:01:38 +0800

    chore:update releases

---
+++ webpack/webpack.dashboard.prod.js
@@ -0,0 +1,57 @@
+//
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const webpackMerge = require('webpack-merge');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const utils = require('./utils');
+const path = require('path');
+const Visualizer = require('webpack-visualizer-plugin');
+
+//引入必要配置
+const baseModuleConfig = require("./config/base.module.config.js");
+const basePluginConfig = require("./config/base.plugin.config.js");
+const commonChunkConfig = require("./config/commonChunk.plugin.config.js");
+const constainsConfig = require("./config/constains.config.js");
+const cssConfig = require("./config/css.config.js");
+const scssConfig = require("./config/scss.config.js");
+const es6Config = require("./config/es6.config.js");
+const entryConfig = require("./config/entry.config.js");
+const externalsConfig = require("./config/externals.config.js");
+const outputConfig = require("./config/output.config.js");
+const resolveConfig = require("./config/resolve.config.js");
+const vueConfig = require("./config/vue.module.config.js");
+
+
+let option = {env: "production", apiURL: ''};
+
+let config = webpackMerge(
+    baseModuleConfig(option),
+    basePluginConfig(option),
+    commonChunkConfig(option),
+    constainsConfig(option),
+    cssConfig(option),
+    scssConfig(option),
+    es6Config(option),
+    entryConfig(option),
+    externalsConfig(option),
+    outputConfig(option),
+    // resolveConfig(option),
+    vueConfig(option),
+    {
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                output: {
+                    comments: false,  // remove all comments
+                },
+                compress: {
+                    warnings: false
+                }
+            }),
+            new Visualizer({
+                // Webpack statistics in target folder
+                filename: '../stats.html'
+            })
+        ]
+    });
+module.exports = config;
