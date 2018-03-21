f2148d5077e6878a4d409d4acc3adf24614fe79b
WANG WEI WEI <vivlanwong120729@gmail.com>
2018-03-21 18:46:26 +0800

    feat:new input style

--- webapp/app/pages/components/InputGroup.vue
+++ webapp/app/pages/components/InputGroup.vue
@@ -1,31 +1,38 @@
 <template>
     <div class="bd-example">
-        <div class="input-group">
-            <span class="input-group-addon" id="basic-addon1">@</span>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">@</span>
+            </div>
             <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
         </div>
-        <br>
-        <div class="input-group">
+        <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <span class="input-group-addon" id="basic-addon2">@example.com</span>
+            <div class="input-group-append">
+                <span class="input-group-text" id="basic-addon2">@example.com</span>
+            </div>
         </div>
-        <br>
         <label for="basic-url">Your vanity URL</label>
-        <div class="input-group">
-            <span class="input-group-addon" id="basic-addon3">https://example.com/users/</span>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
+            </div>
             <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
         </div>
-        <br>
-        <div class="input-group">
-            <span class="input-group-addon">$</span>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text">$</span>
+            </div>
             <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-            <span class="input-group-addon">.00</span>
+            <div class="input-group-append">
+                <span class="input-group-text">.00</span>
+            </div>
         </div>
-        <br>
         <div class="input-group">
-            <span class="input-group-addon">$</span>
-            <span class="input-group-addon">0.00</span>
-            <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+            <div class="input-group-prepend">
+                <span class="input-group-text">With textarea</span>
+            </div>
+            <textarea class="form-control" aria-label="With textarea"></textarea>
         </div>
     </div>
 </template>
