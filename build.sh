6066d5733adaecffab26240cad674dce7684dbb2
Wang <weiwei.wang-ww@siemens.com>
2018-04-23 17:03:32 +0800

    update commit tag to build

--- build.sh
+++ build.sh
@@ -3,7 +3,21 @@
 npm install
 
 if [ "$TRAVIS_TAG" != "" ]; then
-  echo $TRAVIS_TAG
+    node release.js
+    git config --local user.name "travis"  # 推送回gh-pages需要的基本配置
+    git config --local user.email vivlanwong120729@gmail.com
+    git add package.json
+    git commit -m "[auto]Update version to $TRAVIS_TAG"
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:master
+    npm run build
+    cd ../
+    git clone  --depth 50 --branch gh-pages http://github.com/vemex/Nicety.git  nicety-pages
+    cp -r Nicety/target/www/*  nicety-pages
+    cd nicety-pages
+    git add .
+    git commit -m "[auto]Update version to $TRAVIS_TAG"
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages
+    cd ../Nicety
 else
     echo test
 fi 
