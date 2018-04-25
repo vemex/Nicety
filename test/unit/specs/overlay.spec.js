4bdc24da293c7810b321ba6fa662b70758384ab9
Wang <weiwei.wang-ww@siemens.com>
2018-04-25 16:31:28 +0800

    add ut for loadbox in overlay file

--- test/unit/specs/overlay.spec.js
+++ test/unit/specs/overlay.spec.js
@@ -3,10 +3,12 @@ const assert = require('assert');
 window.$ = require('jquery');
 describe('overlay.js', () => {
     it('should render correct contents', () => {
-     var el=$('<div>');
-      var testObj=new overlay(el,{});
-      var ss= testObj._getIconTemplate("rotating-plane");
-      expect( '<div class="sk-rotating-plane"></div>')
-      .to.equal(ss)
-      })
+        var el = $('<div>');
+        var testObj = new overlay(el, {});
+        var ss = testObj._getIconTemplate("rotating-plane");
+        expect('<div class="sk-rotating-plane"></div>')
+            .to.equal(ss)
+        testObj._loadbox(el);
+
+    })
 });
