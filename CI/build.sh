357135ca7b2aac0a3ed89b1f9935f5b95b03df2a
Wang <weiwei.wang-ww@siemens.com>
2018-04-26 14:15:38 +0800

    update CI

--- CI/build.sh
+++ CI/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #set -ev
 
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') 
-echo "$PACKAGE_VERSION"
 npm install
-node global.js
 if [ "$TRAVIS_TAG" != "" ]; then
     node release.js
     git config --local user.name "travis"  # 推送回gh-pages需要的基本配置
