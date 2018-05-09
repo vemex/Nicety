891daef77c1e98eda84eb8893c3c3c24cdffb19c
WANG WEI WEI <vivlanwong120729@gmail.com>
2018-05-09 23:12:29 +0800

    add notify alert

--- src/notification.js
+++ src/notification.js
@@ -1,7 +1,6 @@
 import $ from 'jquery'
 import bsUtil from "bootstrap/js/src/util";
 import nyUtil from "./utils";
-import './content/scss/nicety-notification.scss'
 
 
 const Notification = (($) => {
@@ -17,8 +16,21 @@ const Notification = (($) => {
     const JQUERY_NO_CONFLICT = $.fn[NAME];
 
     const Default = {
-        message: ''
+        message: '',
+        show:function () {
+
+        },close:function () {
+
+        },cancel:function () {
+            return true;
+        },confirm:function () {
+
+        }
     };
+
+    const Selector = {
+        AlertButton: 'button[data-role="alert"]'
+    }
 
     const DefaultType = {
         message: 'string'
@@ -29,28 +41,36 @@ const Notification = (($) => {
         Notify_Alert_div_id: 'notify-alert-div-',
         Notify_Alert_div: 'notify-alert-div'
     };
+    const Event = {
+        CLICK_DATA_API: `click${EVENT_KEY}${DATA_API_KEY}`
+    };
 
     class Notify {
         constructor($el, config) {
             this._element = $el;
             this._config = this._getConfig(config);
+            this._ovId = Class.Notify_Alert_div_id + nyUtil.uID() + nyUtil.uID() + "-" + nyUtil.uID();
         }
 
-        _show(){
-            var target = $('body'),
-                ovId = Class.Notify_Alert_div_id + nyUtil.uID() + nyUtil.uID() + "-" + nyUtil.uID(),
-                panelOv = $('<div id="' + ovId + '" class="' + Class.Notify_Alert_div + '"></div>');
+        _show() {
+            let target = $('body'),
+                panelOv = $('<div id="' + this._ovId + '" class="' + Class.Notify_Alert_div + '"></div>');
             target.addClass(Class.Notify_Alert_Warp);
-            panelOv.appendTo(target).html('<div class="notify-alert-content">'+
-                '<div class="card">'+
-                this._config.message
-                    +'<button type="button" class="btn btn-outline-primary">Primary</button>\n' +
-                '      <button type="button" class="btn btn-outline-secondary">Secondary</button></div>'
+            panelOv.appendTo(target).html('<div class="notify-alert-content">' +
+                '<div class="">'
+                + '<div class="alert-message">' + this._config.message + '</div>'
+                + '<div class="alert-operation">'
+                + '<button type="button" data-role="alert" class="alert-cancel">Cancel</button>\n' +
+                '<button type="button" data-role="alert" class="alert-confirm">Confirm</button>'
+                + '</div>'
+                + '</div>'
                 + '</div>');
+            this._config.show.call(this);
         }
 
-        _close(){
-
+        _close() {
+            $('#' + this._ovId).remove();
+            this._config.close.call(this);
         }
 
         _getConfig(config) {
@@ -69,43 +89,59 @@ const Notification = (($) => {
             this._element = null;
             this._config = null;
         }
+
         static get Version() {
             return VERSION;
         }
 
         static _jQueryInterface(config) {
-            return this.each(function() {
-                if (config === undefined) {
-                    config = {}
-                }
-                const $element = $(this);
-                let data = $element.data(DATA_KEY);
-                let isInit = !data || (typeof config === "object" && config);
+            if (config === undefined) {
+                config = {}
+            }
+            const $element = $('body');
+            let data = $element.data(DATA_KEY);
+            let isInit = !data || (typeof config === "object" && config);
 
-                if (isInit) {
-                    if (data) {
-                        data._close($element);
-                        data.dispose();
-                    }
-                    if ($element.attr('data-target')) {
-                        config.target = $element.attr('data-target');
-                    }
-                    data = new Notify($element, config);
-                    $element.data(DATA_KEY, data);
-                    data['_show']($element);
+            if (isInit) {
+                if (data) {
+                    data._close($element);
+                    data.dispose();
                 }
+                if ($element.attr('data-target')) {
+                    config.target = $element.attr('data-target');
+                }
+                data = new Notify($element, config);
+                $element.data(DATA_KEY, data);
+                data['_show']($element);
+            }
 
-                if (typeof config == 'string') {
-                    data[config]($element)
-                }
-            })
-        }
+            if (typeof config == 'string') {
+                data[config]($element)
+            }
+        };
     }
 
-    $.fn[NAME] = Notify._jQueryInterface;
-    $.fn[NAME].Constructor = Notify;
-    $.fn[NAME].noConflict = function() {
-        $.fn[NAME] = JQUERY_NO_CONFLICT;
+    $(document).on(
+        Event.CLICK_DATA_API,
+        Selector.AlertButton,
+        function () {
+            let data = $('body').data(DATA_KEY);
+            if ($(this).hasClass('alert-cancel')) {
+                let cancelResult=data._config.cancel.call(data);
+                if (cancelResult===undefined || cancelResult){
+                    data._close();
+                }
+            }
+            if ($(this).hasClass('alert-confirm')){
+                data._config.confirm.call(data);
+            }
+        }
+    );
+
+    $[NAME] = Notify._jQueryInterface;
+    $[NAME].Constructor = Notify;
+    $[NAME].noConflict = function () {
+        $[NAME] = JQUERY_NO_CONFLICT;
         return Notify._jQueryInterface
     };
 
