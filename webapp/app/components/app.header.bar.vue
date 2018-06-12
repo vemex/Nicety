35766fcc727384a98051b451b0a98f19a3cb42af
WANG WEI WEI <vivlanwong120729@gmail.com>
2018-06-13 00:14:13 +0800

    adjust headerbar

--- webapp/app/components/app.header.bar.vue
+++ webapp/app/components/app.header.bar.vue
@@ -1,46 +1,50 @@
 <template>
-    <div>
-        <div class="dropdown">
-            <button class="dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                <i class="pli-bell"></i><span class="badge">4</span>
-            </button>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2"
-                 x-placement="bottom-start"
-                 style="position: absolute; transform: translate3d(0px, 29px, 0px); top: 0px; left: 0px; will-change: transform;">
-                <form class="px-4 py-3">
-                    <div class="form-group">
-                        <label for="exampleDropdownFormEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleDropdownFormEmail1"
-                               placeholder="email@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleDropdownFormPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleDropdownFormPassword1"
-                               placeholder="Password">
-                    </div>
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="dropdownCheck">
-                        <label class="form-check-label" for="dropdownCheck">
-                            Remember me
-                        </label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </form>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">New around here? Sign up</a>
-                <a class="dropdown-item" href="#">Forgot password?</a>
-            </div>
+    <div class="d-flex justify-content-between">
+        <div class="left-container">
+
         </div>
-        <button type="button" data-container="body" data-toggle="popover" data-trigger="hover"
-                data-placement="bottom"
-                data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
-            <i class="pli-mail-2"></i>
-        </button>
-        <button type="button" data-container="body" data-toggle="popover" data-trigger="hover"
-                data-placement="bottom" data-content="退出">
-            <i class="pli-unlock"></i>
-        </button>
+        <div class="right-container">
+            <div class="dropdown">
+                <button class="dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    <i class="pli-bell"></i><span class="badge">4</span>
+                </button>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2"
+                     x-placement="bottom-start"
+                     style="position: absolute; transform: translate3d(0px, 29px, 0px); top: 0px; left: 0px; will-change: transform;">
+                    <form class="px-4 py-3">
+                        <div class="form-group">
+                            <label for="exampleDropdownFormEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleDropdownFormEmail1"
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleDropdownFormPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleDropdownFormPassword1"
+                                   placeholder="Password">
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="dropdownCheck">
+                            <label class="form-check-label" for="dropdownCheck">
+                                Remember me
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign in</button>
+                    </form>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">New around here? Sign up</a>
+                    <a class="dropdown-item" href="#">Forgot password?</a>
+                </div>
+            </div>
+            <button type="button" data-container="body" data-toggle="popover" data-trigger="hover"
+                    data-placement="bottom"
+                    data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+                <i class="pli-mail-2"></i>
+            </button>
+            <button type="button" data-container="body" data-toggle="popover" data-trigger="hover"
+                    data-placement="bottom" data-content="退出">
+                <i class="pli-unlock"></i>
+            </button>
+        </div>
     </div>
 </template>
 
