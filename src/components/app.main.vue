fdaeff14f3ac31fec535511a5e9037e210a0d03f
WANG WEI WEI <vivlanwong120729@gmail.com>
2018-10-02 14:29:24 +0800

    add no tab main

---
+++ src/components/app.main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="page-title">
+            <app-breadcrumb></app-breadcrumb>
+        </div>
+        <div class="page-content">
+            <div>
+                <router-view></router-view>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script>
+    import AppBreadcrumb from "./app.breadcrumb"
+
+    export default {
+        components: {
+            AppBreadcrumb
+        }
+    }
+</script>
